47c99cb580049ee7faea5c3e3e7341ad223be99e
Patrizia Busatto <patribu7@gmail.com>
2023-10-10 14:09:02 +0200

    Create the board

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,18 @@
 <script setup>
-
+const board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 </script>
 
 
 <template>
-    <div>I am the board game</div>
+    <div v-for="(row, iRow) of board" :key="iRow">
+        <button class="cell" v-for="(cell, iCell) of row" :key="iCell">{{ cell }}</button>
+    </div>
 </template>
 
 
-<style></style>
+<style>
+.cell {
+    width: 200px;
+    height: 200px;
+}
+</style>
