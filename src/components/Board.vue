e62a767571b57269ac41c8e1c4d99a7db82bdf0d
Patrizia Busatto <patribu7@gmail.com>
2023-10-10 14:15:06 +0200

    Put mark dinamically

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,22 @@
 <script setup>
-const board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+import { ref } from 'vue'
+
+const board = ref([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+const turn = ref(1)
+
+function move(iCell, iRow) {
+  let mark = (turn.value % 2 === 0) ? '🔵' : '❌'
+
+  board.value[iRow][iCell] = mark
+  turn.value++
+}
+
 </script>
 
 
 <template>
     <div v-for="(row, iRow) of board" :key="iRow">
-        <button class="cell" v-for="(cell, iCell) of row" :key="iCell">{{ cell }}</button>
+        <button class="cell" @click="move(iCell, iRow)" v-for="(cell, iCell) of row" :key="iCell">{{ cell }}</button>
     </div>
 </template>
 
