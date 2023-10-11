a2cf2ea8b2f040664d35dd97d9e3ae7cea56445e
Patrizia Busatto <patribu7@gmail.com>
2023-10-11 11:20:39 +0200

    Add win condition

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,21 +1,48 @@
 <script setup>
 import { ref, computed } from 'vue'
+class Cell {
+    constructor(mark, n) {
+        this.mark = mark;
+        this.n = n
+    }
+}
 
-const board = ref([['', '', ''], ['', '', ''], ['', '', '']])
+const board = ref([[new Cell('', [1, 4, 7]), new Cell('', [1, 5]), new Cell('', [1, 6, 8]),], [new Cell('', [2, 4]), new Cell('', [2, 5, 7, 8]), new Cell('', [2, 6]),], [new Cell('', [3, 4, 8]), new Cell('', [3, 5]), new Cell('', [3, 6, 7]),]])
+
 const turn = ref(0)
 const moveOfX = ref([])
 const moveOfO = ref([])
+let winner = ref(false)
+
+
+
 
 function move(iCell, iRow) {
     if (turn.value % 2 === 0) {
         let mark = '❌';
-        board.value[iRow][iCell] = mark
-        moveOfX.value.push([iRow, iCell]);
-
+        board.value[iRow][iCell].mark = mark
+        moveOfX.value.push(...board.value[iRow][iCell].n);
+        moveOfX.value.forEach(el => {
+            var itemsFound = moveOfX.value.filter(function (e) {
+                return e == el;
+            }).length;
+            if (itemsFound == 3) {
+                winner.value = 'X'
+              
+            }
+        });
     } else {
         let mark = '🔵';
-        board.value[iRow][iCell] = mark;
-        moveOfO.value.push([iRow, iCell]);
+        board.value[iRow][iCell].mark = mark;
+        moveOfO.value.push(...board.value[iRow][iCell].n);
+        moveOfO.value.forEach(el => {
+            var itemsFound = moveOfO.value.filter(function (e) {
+                return e == el;
+            }).length;
+            if (itemsFound == 3) {
+                winner.value = 'O'
+            }
+        });
     }
     turn.value++
 }
@@ -24,11 +51,14 @@ function move(iCell, iRow) {
 
 
 <template>
+    <div v-if="winner">the winner is {{ winner }}</div>
     <div v-for="(row, iRow) of board" :key="iRow">
-        <button class="cell" @click="move(iCell, iRow)" v-for="(cell, iCell) of row" :key="iCell" :disabled="cell != ''">{{
-            cell }}</button>
+        <button class="cell" @click="move(iCell, iRow)" v-for="(cell, iCell) of row" :key="iCell"
+            :disabled="cell.mark != ''">{{
+                cell.mark }}</button>
     </div>
     <div>move of X{{ moveOfX }} / move of O{{ moveOfO }}</div>
+    <div>{{ turn }}</div>
 </template>
 
 
