326b9a0e17252221cc39015eb6fa1bc2a16bd4d6
Patrizia Busatto <patribu7@gmail.com>
2023-10-15 13:50:19 +0200

    Player Chooser styled

--- src/components/PlayerChooser.vue
+++ src/components/PlayerChooser.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue'
 const props = defineProps(['firstPlayer'])
 
 
@@ -7,9 +6,34 @@ const props = defineProps(['firstPlayer'])
 </script>
 
 <template>
-<div class="player-chooser">X play first. The player who has X is on the {{ firstPlayer }}</div>
+    <div class="player-chooser">
+        <p>
+            X play first. <br>
+            The player who has X is on the <br>
+            {{ firstPlayer }}
+        </p>
+        <p>...</p>
+        
+    </div>
 </template>
 
 <style scoped>
+.player-chooser {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    bottom: 50%;
+    right: 35%;
+    width: 300px;
+    height: 200px;
+    background-color: #e8e4d6;
+    
+}
+
+
+
 
 </style>
